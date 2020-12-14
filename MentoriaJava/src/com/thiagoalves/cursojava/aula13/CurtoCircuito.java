c5b88385c408f010c997f299afecefd7b07bfbac
Thiago Alves <thiago.balves@hotmail.com>
2020-12-13 21:50:02 -0300

    Aula 13 - Operadores

---
+++ MentoriaJava/src/com/thiagoalves/cursojava/aula13/CurtoCircuito.java
@@ -0,0 +1,18 @@
+package com.thiagoalves.cursojava.aula13;
+
+public class CurtoCircuito {
+	
+	public static void main(String[] args) {
+		
+		boolean verdadeiro = true;
+		boolean falso = false;
+		boolean resultado1 = falso & verdadeiro;
+		//ao ultilizar o curto circuito && quando o primeiro dado é falso, ele ignora a comparação com o segundo dado, pois o resultado será falso.
+		boolean resultado2 = falso && verdadeiro; 
+		
+		System.out.println(resultado1);
+		System.out.println(resultado2);
+		
+	}
+
+}
---
+++ MentoriaJava/src/com/thiagoalves/cursojava/aula13/OrdemPrecedencia.java
@@ -0,0 +1,15 @@
+package com.thiagoalves.cursojava.aula13;
+
+public class OrdemPrecedencia {
+	
+	public static void main(String[] args) {
+		
+		//1º multiplicação
+		//2º divisão
+		//3º restante da operação
+		int resultado = 1 + 1 - 1 + 1 * 1 / 1;
+		
+		System.out.println(resultado);
+	}
+
+}
